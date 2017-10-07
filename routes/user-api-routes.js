9fda41d85c9f18c7aec49d94050470ae8ddc9fc7
Zachary Saltzman <zsaltzman@zacharys-mbp-2.broward.edu>
2017-10-07 13:28:45 -0400

    updated api routes for user

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,26 +1,46 @@
 var db = require("../models");
 
 module.exports = function(app) {
+    var query = {}
     app.get("/api/users", (req, res) => {
-        db.User.findAll({}).then((dbUser) => {
+        db.User.findAll({
+            where: query,
+            include: [{
+                model: db.Bet
+            }]
+        }).then((dbUser) => {
             res.json(dbUser);
         });
     });
 
     app.get("/api/users/:id," (req, res) => {
-        db.User.findOne(req.params.id).then((dbUser) => {
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: db.Bet,
+                where: {
+                  userID: req.params.id
+                }
+            }]
+        }).then((dbUser) => {
             res.json(dbUser);
         });
     });
 
     app.post("/api/users/", (req, res) => {
-        db.User.insertOne(req.body.name, req.body.password).then((dbUser) => {
+        db.User.create(req.body).then((dbUser) => {
             res.json(dbUser);
         });
     });
 
     app.delete("/api/users/:id", (req, res) => {
-        db.User.destroy(req.params.id).then((dbUser) => {
+        db.User.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then((dbUser) => {
             res.json(dbUser);
         });
     });
