1b8a82a76dae4dc1e7fea1da50a2f7f86bd3b5b4
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-10-12 18:51:47 -0400

    Added handlebars each loop

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,2 @@
-var indexTemplateScript = $("#index-template").html();
 
-var iTemplate = Handlebars.compile(indexTemplateScript);
 
-var data = 
-
-$(document.body).append(iTemplate(data));
-
-var signInTemplateScript = $("#sign-in-template").html();
-
-var sTemplate = Handlebars.compile(signInTemplateScript);
-
-var data = 
-
-$(document.body).append(sTemplate(data));
