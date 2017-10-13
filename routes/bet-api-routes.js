00ba9638c9c3a69b4e36bc483eeef9ad409764c0
Zachary Saltzman <zsaltzman@Zacharys-MBP-2.hsd1.fl.comcast.net>
2017-10-13 01:40:36 -0400

    userbets

--- routes/bet-api-routes.js
+++ routes/bet-api-routes.js
@@ -61,4 +61,72 @@ module.exports = function(app) {
             res.redirect("/fightcards");
         });
     });
+
+    app.post("/karma/:id", (req, res) => {
+        db.Bet.findOne({
+                where: {
+                    id: req.params.id
+                },
+                include: [{
+                    model: db.User
+                },
+                {
+                    model: db.Match
+                }]
+            }).then((dbBet)=> {
+                if (dbBet.Match.result == "TBD") {
+                    res.redirect("/userbets");
+                }
+                else {
+            db.Bet.update({
+                    karma_received: true
+                },
+                {
+                    where: {
+                        id: req.params.id
+                    }
+            }).then((dbBet) => {
+                db.Bet.findOne({
+                    where: {
+                        id: req.params.id
+                    },
+                    include: [{
+                        model: db.User
+                    },
+                    {
+                        model: db.Match
+                    }]
+                }).then((dbBet)=> {
+                    var betAmount = dbBet.amount;
+                    var oldAmount = dbBet.User.karma;
+                    var newKarma = 0;
+                    if (dbBet.Match.result == "Win") {
+                        newKarma = parseFloat(betAmount) + parseFloat(oldAmount);
+                    }
+                    else if (dbBet.Match.result == "Loss") {
+                        newKarma = parseFloat(oldAmount) - parseFloat(betAmount);
+                    }
+                    else if (dbBet.Match.result == "Draw") {
+                        newKarma = parseFloat(oldAmount);
+                    }
+                    else {
+                        newKarma = parseFloat(oldAmount);
+                    }
+                    db.User.update({
+                            karma: newKarma
+                        },
+                        {
+                            where: {
+                                id: req.session.userid
+                            }
+                    }).then((dbUser) => {
+                        req.session.karma = newKarma;
+                        res.redirect("/userbets");
+                    });
+                });                     
+            });
+             }
+        })
+    });
+
 }
